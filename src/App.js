8bd8e44e9e43e30848b6e833efad99f8a7df04a9
dv-kong <zektbach@outlook.com>
2021-04-14 17:13:58 +0200

    api + recipe-details wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-//dkong
-import {HeaderRecipeDetails} from './components/Header'
-import {RecipeDetails, RecipeCard} from './components/Recipes'
-//
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import HeaderRecipeDetails  from "./components/Header"; //dkong
+import { RecipeDetails, RecipeCard } from "./components/Recipes"; //
+
 function App() {
   return (
     <div className="App">
@@ -21,12 +23,28 @@ function App() {
           Learn React
         </a>
       </header> */}
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/recipe-details">Recipe Details</Link>
+          </li>
+        </ul>
 
-      <HeaderRecipeDetails/>
-      <RecipeDetails/>
-      ------------------------------------------------------------------------------------------------
-      <RecipeCard/>
-      
+        <Switch>
+          <Route path="/recipe-details">
+            <HeaderRecipeDetails />
+            <RecipeDetails />
+            {/* <RecipeDetails prop={prop} /> */}
+          </Route>
+          <Route path="/">CONTENTS OF Homepage</Route>
+        </Switch>
+      </Router>
+
+      {/* ------------------------------------------------------------------------------------------------ */}
+      {/* <RecipeCard /> */}
     </div>
   );
 }
--- src/components/Recipes/RecipeDetails.jsx
+++ src/components/Recipes/RecipeDetails.jsx
@@ -1,22 +1,47 @@
 import React from "react";
+import axios from "axios";
 
+// e.preventDefault
 class RecipeDetails extends React.Component {
+  state = {
+    apiGetRes: [],
+    apiRes: [],
+    mealObject: {},
+  };
+
+  componentDidMount() {
+    // www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+    axios
+      .get("https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+
+      .then((response) => {
+        // console.log(`+++RESULT:`, response.data.meals[0]);
+        const mealObject = response.data.meals[0];
+        console.log(mealObject);
+
+        this.setState({ mealObject: mealObject });
+      })
+
+      .catch((error) => console.error(error.message)); // USELESS CODE?
+  }
+
   render() {
     return (
       <section>
-        <h3>Recipe</h3>
-        <p>Description of Recipe</p>
+        <figure>
+          <img
+            src={this.state.mealObject.strMealThumb}
+            alt={this.state.mealObject.strMeal}
+          />
+        </figure>
+        <h3>{this.state.mealObject.strMeal}</h3>
+        <p></p>
+        <p>Instructions/Steps:</p>
         <ul>
-          <li>Instruction / Step</li>
-          <li>Instruction / Step</li>
-          <li>Instruction / Step</li>
-          <li>Instruction / Step</li>
-          <li>Instruction / Step</li>
+          {this.state.mealObject.strInstructions}
+
         </ul>
         <div>
-          <figure>
-            <img src="" alt="Recipe Image" />
-          </figure>
           <button>Add to Favorites</button>
         </div>
       </section>
