799cd9434bff47a491dd8183439be266f9ace73a
dv-kong <zektbach@outlook.com>
2021-04-17 22:47:45 +0200

    save before pull

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-<<<<<<< HEAD
-import './App.scss';
-import Hero from './pages/Hero/hero';
-import HomeContent from './pages/Homepage/homepage';
-import RecipesByCountries from './pages/RecipesByCountries';
-import Categories from './pages/Categories/categories';
-import RecipesByCategories from './pages/RecipesByCategories';
-import RecipesByIngredients from './pages/RecipesByIngredients';
-import DetailsRecipe from './pages/DetailsRecipe';
-import Favoris from './pages/Favoris';
-import { Navbar } from './components/Navbar';
+import "./App.scss";
+import Hero from "./pages/Hero/hero";
+import HomeContent from "./pages/Homepage/homepage";
+import RecipesByCountries from "./pages/RecipesByCountries";
+import Categories from "./pages/Categories/categories";
+import RecipesByCategories from "./pages/RecipesByCategories";
+import RecipesByIngredients from "./pages/RecipesByIngredients";
+import DetailsRecipe from "./pages/DetailsRecipe";
+import Favoris from "./pages/Favoris";
+import { Navbar } from "./components/Navbar";
 // Vincent
-import {HeaderRecipeDetails} from './components/Header'
-import {RecipeDetails, RecipeCard} from './components/Recipes'
+import { HeaderRecipeDetails } from "./components/Header";
+import { RecipeDetails, RecipeCard } from "./components/Recipes";
+import RecipeDetailsPage from "./Pages/DetailsRecipe";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { useHistory } from "react-router-dom";
 
 function App() {
+  let history = useHistory();
+  console.log(`history: `, history);
+
   return (
     <>
       <Router>
-
         <Navbar />
 
         <Switch>
           <Route path="/Hero" exact component={Hero} />
           <Route path="/Homepage" exact component={HomeContent} />
-          <Route path="/RecipesByCountries" exact component={RecipesByCountries} />
-          <Route path="/RecipesByCategories" exact component={RecipesByCategories} />
+          <Route
+            path="/RecipesByCountries"
+            exact
+            component={RecipesByCountries}
+          />
+          <Route
+            path="/RecipesByCategories"
+            exact
+            component={RecipesByCategories}
+          />
           <Route path="/Categories" exact component={Categories} />
-          <Route path="/RecipesByIngredients" exact component={RecipesByIngredients} />
+          <Route
+            path="/RecipesByIngredients"
+            exact
+            component={RecipesByIngredients}
+          />
           <Route path="/DetailsRecipes" exact component={DetailsRecipe} />
           {/* <HeaderRecipeDetails/>
       <RecipeDetails/>
@@ -36,64 +52,27 @@ function App() {
 
           <Route path="/Favoris" exact component={Favoris} />
         </Switch>
+        <li>
+          <Link to="/recipe-details">Recipe Details</Link>
+        </li>
 
-      </Router>
-    </>
+        <Route
+          exact
+          path="/recipe-details"
+          component={RecipeDetailsPage}
+        ></Route>
 
-
-=======
-import logo from "./logo.svg";
-import "./App.css";
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import HeaderRecipeDetails from "./components/Header"; //dkong
-import { RecipeDetails, RecipeCard } from "./components/Recipes"; //
-import RecipeDetailsPage from "./Pages/DetailsRecipe";
-// import DetailsRecipeContext from "./contexts/DetailsRecipeContext";
-// import React, {Component} from 'react';
-
-// const ExampleContext = React.createContext('light'); // test
-
-import { useHistory } from "react-router-dom";
-
-function App() {
-  // let history = useHistory();
-  let history = useHistory();
-  console.log(`history: `, history);
-
-  return (
-    <div className="App">
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/recipe-details">Recipe Details</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route exact path="/recipe-details" component={RecipeDetailsPage}>
-            {/* <ExampleContext.Provider value={"hey"}> */}
-            {/* <HeaderRecipeDetails />
+        {/* <ExampleContext.Provider value={"hey"}> */}
+        {/* <HeaderRecipeDetails />
               <RecipeDetails /> */}
 
-            {/* {this.value} */}
-            {/* </ExampleContext.Provider> */}
-          </Route>
-          <Route exact path="/category/categoryName">
-            Category (this.state?)
-          </Route>
-          <Route path="/">Homepage</Route>
-        </Switch>
+        {/* {this.value} */}
+        {/* </ExampleContext.Provider> */}
+        <Route exact path="/category/categoryName">
+          Category (this.state?)
+        </Route>
       </Router>
-
-      {/* ------------------------------------------------------------------------------------------------ */}
-      {/* <RecipeCard /> */}
-    </div>
->>>>>>> api + recipe-details wip
+    </>
   );
 }
 
